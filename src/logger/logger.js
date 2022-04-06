bf498cb28390a479cbc9a99718d4802327fe762f
hempalS39 <hs527196@gmail.com>
2022-04-07 03:01:52 +0530

    hi

---
+++ src/logger/logger.js
@@ -0,0 +1,5 @@
+let log = function(){
+    console.log("Welcom to my application. I am <name> and I am a part of FunctionUp Uranium cohort")
+}
+
+module.exports.logging = log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const logger = require('../logger/logger')
+
+const util = require('../util/helper')
+ const validator = require('../validator/formatter')
+  
+ const array = require('../array/arr')
 
 const router = express.Router();
 
+
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
-});
+    
+    // console.log("calling the function")
+    logger.logging
+    util.printDate()
+    util.printMonth()
+    util.a()
+    validator.trim
+    validator.lowercase
+    validator.uppercase
+    array.result
+    array.result1
+
+
+})
+// router.get('/test-me', function (req, res) {
+//     res.send('My first ever api!')
+    
+//     console.log("calling the function")
+//    util.a()
+// })
+
 
 module.exports = router;
 // adding this comment for no reason
