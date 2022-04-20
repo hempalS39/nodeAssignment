ed971747187475fbf5b1119bb343f5aacfeab7f1
hempalS39 <hs527196@gmail.com>
2022-04-20 16:54:53 +0530

    assignment19aprl

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,26 +1,28 @@
 
 const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
+    let ar1 = new Date().toLocaleString();
+    let ar2 = req.ip
+    let url = req.originalUrl
+    console.log(ar1,ar2,url)
     next()
 }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
-}
+// const mid2= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid2")
+//     next()
+// }
 
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
+// const mid3= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid3")
+//     next()
+// }
 
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
+// const mid4= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid4")
+//     next()
+// }
 
 module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+// module.exports.mid2= mid2
+// module.exports.mid3= mid3
+// module.exports.mid4= mid4
